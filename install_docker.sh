868f9dd5c258e0432114a378fac4ef079028c418
SwaggerBehemoth <swagger.behemoth@gmail.com>
2024-10-16 14:14:45 +0000

    Ran dos2unix utility on the installation script to remove CRLF characters. Corrected the docker compose command

--- install_docker.sh
+++ install_docker.sh
@@ -1,12 +1,12 @@
-#/bin/bash
+#!/usr/bin/bash
 
 sudo apt-get update
-sudo apt-get  install expect -y
+sudo apt-get install expect -y
 
-echo "1st step - Install dependencies"
+echo -e "\n\n\n1st step - Install dependencies\n\n"
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 
-echo "2nd step - Add the official Docker GPG key"
+echo -e "\n\n2nd step - Add the official Docker GPG key\n\n"
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,7 +15,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 ##  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -    ##
 
-echo  "3rd Step - Add the Docker repository"
+echo -e  "\n\n3rd Step - Add the Docker repository\n\n"
 
 
 echo \
@@ -23,17 +23,15 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-##  yes " " | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"  ##
-#   apt-cache policy docker-ce  #
+sudo apt-get update
 
-echo  "4th Step - Install Docker Engine, CLI and docker compose plugin"
+echo -e  "\n\n4th Step - Install Docker Engine, CLI and docker compose plugin\n\n"
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-##  sudo apt-get install docker-ce -y       ##
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-echo "5th Step - Add the current user to the 'docker' group to run Docker commands without 'sudo'"
+echo -e "\n\n5th Step - Add the current user to the 'docker' group to run Docker commands without 'sudo'\n\n"
 
 sudo usermod -aG docker ${USER}
-echo "Succesfully installed docker version " $(docker --version)
+echo -e "\n\nSuccesfully installed docker version " $(docker --version)
 
-echo "Succesfully installed docker compose  version " $(docker-compose --version)
+echo -e "\n\nSuccesfully installed docker compose  version " $(docker compose version)
